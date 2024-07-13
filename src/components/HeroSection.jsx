b25891985a8d29999265651654c1640c8dd06e9a
Jonathan Andersson <2jonand@gmail.com>
2024-07-13 02:28:05 +0200

    simple animation and some padding changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -20,8 +20,8 @@ function HeroSection({ scrollToContact, scrollToStack, contactRef, heroRef, stac
     <div ref={heroRef} className="hero-wrapper">
       <div className="hero-content">
         <div className="heading">
-          <h1 className="hero-heading">Hi! I'm <span>Jonathan</span>, a Design Inspired Full-Stack Developer</h1>
-          <div className="btn-container">
+          <h1 className="hero-heading drop-in-1">Hi! I'm <span>Jonathan</span>, a Design Inspired Full-Stack Developer</h1>
+          <div className="btn-container drop-in-2">
             <button onClick={scrollToStack} className="sec-btn">&darr;</button>
             <button onClick={scrollToContact} className={isScrolled ? "prim-btn hidden" : "prim-btn visible" }>Contact me</button>
           </div>
