1c8c2997f24f341a56b594a95eaba21e4646ff47
Jonathan Andersson <2jonand@gmail.com>
2024-12-29 12:30:54 +0100

    added recent projects

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,10 +1,11 @@
 import "../assets/style/components/CardSlider.scss";
 import "../assets/style/components/HeroSection.scss";
-import sBookster from "../assets/images/bookster-preview.png";
 import sLego from "../assets/images/lego-preview.png";
 import sFredag from "../assets/images/fredags-preview.png";
 import sModerat from "../assets/images/emoderaterna-preview.png";
 import sBowling from "../assets/images/eskilstunabowling-preview.png";
+import sHallbar from "../assets/images/hallbarservice-preview.png";
+import sLeads from "../assets/images/leadsgenie-preview.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from "react";
@@ -15,6 +16,22 @@ function CardSlider({ projectsRef }) {
   const cards = [
     {
       id: 1,
+      title: "Hållbar Fastighetsservice Syd",
+      description: "This website is developed by me as a freelance project for a client. I took care of designing and branding the website according to the clients wishes, as well as SEO and content for the website. The website is built with React.JS.",
+      preview: "https://www.hallbarservice.se",
+      code: "",
+      image: sHallbar,
+    },
+    {
+      id: 2,
+      title: "Leadsgenie",
+      description: "A website I built as a freelance project for a client. Developed as a custom theme for WordPress based on the design provided by Designit Media.",
+      preview: "",
+      code: "",
+      image: sLeads,
+    },
+    {
+      id: 3,
       title: "Brick Canvas",
       description: "Brick Canvas is a service where a user can upload their image, edit it and turn it into a Lego-like mosaic. The service is built with React.js and uses Canvas for the image editing and processing.",
       preview: "https://wztrbrg.github.io/brick-canvas/",
@@ -22,7 +39,7 @@ function CardSlider({ projectsRef }) {
       image: sLego,
     },
     {
-      id: 2,
+      id: 4,
       title: "Eskilstuna Bowlingcenter",
       description: "During my internship I got the chance to update the website for Eskilstuna Bowlingcenter. At first I worked alone but later on the team collaborated to finish the project. Built mainly using WordPress and Elementor PRO, with some custom components built php.",
       preview: "https://eskilstunabowling.se/",
@@ -30,7 +47,7 @@ function CardSlider({ projectsRef }) {
       image: sBowling,
     },
     {
-      id: 3,
+      id: 5,
       title: "Eskilstunamoderaterna",
       description: "Eskilstunamoderaterna is a website that is built on WordPress with Elementor PRO and utilizes the Bedrock boilerplate with Deployer. This project is for a real client and was made during my internship.",
       preview: "https://eskilstunamoderaterna.se",
@@ -38,7 +55,7 @@ function CardSlider({ projectsRef }) {
       image: sModerat,
     },
     {
-      id: 4,
+      id: 6,
       title: "Fredagskollegiet",
       description: "Fredagskollegiet is a single-page website that is built on WordPress with Elementor PRO and utilizes the Bedrock boilerplate with Deployer. This project is for a real client and was made during my internship.",
       preview: "https://fredagskollegiet.se",
